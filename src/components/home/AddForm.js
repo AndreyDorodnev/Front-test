d23b4404c5d593fdc0d2946a2ab3672988c68ce5
Andrey Dorodnev <andrew992@mail.ru>
2020-06-25 15:45:38 +0300

    add card to adduser form

--- src/components/home/AddForm.js
+++ src/components/home/AddForm.js
@@ -1,14 +1,7 @@
 import React, {useState} from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
 
 export default props => {
 
@@ -24,30 +17,33 @@ export default props => {
             first_name: firstName,
             last_name: lastName,
             password: password
-        })
+        });
     }
 
     return (
-        <Form className='edit-form' onSubmit={addFormSubmit}>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" required placeholder="Enter email" value={email} onChange={e=>setEmail(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>First name</Form.Label>
-                <Form.Control type="text" required placeholder="First Name" value={firstName} onChange={e=>setfirstName(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control type="text" required placeholder="Last Name" value={lastName} onChange={e=>setlastName(e.target.value)}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" required placeholder="Password" value={password} onChange={e=>setpassword(e.target.value)}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Add user
-            </Button>
-        </Form>
+        <Card>
+            <Card.Header>Add user</Card.Header>
+            <Form className='edit-form' onSubmit={addFormSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" required placeholder="Enter email" value={email} onChange={e=>setEmail(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control type="text" required placeholder="First Name" value={firstName} onChange={e=>setfirstName(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control type="text" required placeholder="Last Name" value={lastName} onChange={e=>setlastName(e.target.value)}/>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" required placeholder="Password" value={password} onChange={e=>setpassword(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add user
+                </Button>
+            </Form>
+        </Card>
     )
 }
