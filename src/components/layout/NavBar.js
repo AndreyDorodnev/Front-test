589acc4a8a914abbe2bda7ddeceb9221d4a27421
Andrey Dorodnev <andrew992@mail.ru>
2020-06-25 17:24:01 +0300

    nav bar sign out btn fix

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import AuthUserContext from '../session/context';
 
 export default props => {
+    const [user] = useContext(AuthUserContext);
     return (
         <Nav>
-            <Button variant="primary" size="sm" onClick={props.btnClick}>SignOut</Button>
+            <h3>React test app</h3>
+            {
+                user?
+                <Button variant="primary" size="sm" onClick={props.btnClick}>SignOut</Button>:
+                null               
+            }  
         </Nav>
     )
 }
