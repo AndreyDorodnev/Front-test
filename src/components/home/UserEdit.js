2c847a4d145a8416c3228b746c223b37cc73346b
Andrey Dorodnev <andrew992@mail.ru>
2020-06-25 17:16:29 +0300

    clear imports

--- src/components/home/UserEdit.js
+++ src/components/home/UserEdit.js
@@ -1,14 +1,4 @@
-import React, {useState} from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+import React from 'react';
 import EditForm from './EditForm';
 import AddForm from './AddForm';
 
