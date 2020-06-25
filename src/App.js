2af39487ddbf1ad6d9ddddadf1ab5d1729027ba3
Andrey Dorodnev <andrew992@mail.ru>
2020-06-25 13:43:01 +0300

    add sign out method

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Login from './components/auth/Login';
 import NavBar from './components/layout/NavBar';
 import Home from './components/home/Home';
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
-import {loadState} from './localStorage';
+import {loadState,clearState} from './localStorage';
 import AuthUserContext from './components/session/context';
 
 function App() {
 
   const [user,setUser] = useState(loadState());
 
+  const signOut = () => {
+    setUser(null);
+    clearState();
+  }
+
   return (
     <AuthUserContext.Provider value={[user,setUser]}>
-      {console.log('USER',user)
-      }
-    <div>
-      <NavBar/>
+    <div className="main" >
+      <NavBar btnClick={signOut}/>
       <Router>
         <Switch>
           <Route exact path='/' component={Home}/>
